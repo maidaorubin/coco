24245de9124c58e2c29f98de1077964cd166580a
maidaorubin <maidaorubin@163.com>
2015-10-15 13:08:55 +0800

    Rescan  显示--汉字乱码

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # coco
 这是我的第一个GitHub项目，边学习边提高
 By maidaorubin
+
+2015-10-15   12：51  测试更新 
+通过软件 Git copy from github 成功
+
+现在测试  从软件 Git update to  Github  待测  12：52 by 麦刀
+
+
+13:00 上传测试成功。软件显示乱码，但是上传网站正常。
--- hello.py
+++ hello.py
@@ -3,3 +3,4 @@ Print "Hello World!"
 print "I am Rubin.I am coming!"
 print 'I try change the file from the Git windows client 22:00 10/12'
 print 'update 22:12 hehe  on the website'
+#2015-10-15  13:00  by Maidao update
